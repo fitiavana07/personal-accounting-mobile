ab94c65ca2d51ab38d69b5090d00230aea817458
Fitiavana Ramanandafy <hello@fitiavana.dev>
2026-07-29 18:08:08 +0300

    Bump version to 1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.fitiavana.accounting"
         minSdk = 19
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.8"
+        versionCode = 9
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
