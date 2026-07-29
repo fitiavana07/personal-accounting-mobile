f18b89ad962c85324c14d50259b3d44b16798f0e
Fitiavana Ramanandafy <hello@fitiavana.dev>
2026-07-30 01:07:06 +0300

    chore: bump version to 1.10 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.fitiavana.accounting"
         minSdk = 19
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.9"
+        versionCode = 10
+        versionName = "1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
