bb483dd5a3fc2c56417c4df6e9b84761690591e1
Fitiavana Ramanandafy <hello@fitiavana.dev>
2026-08-20 22:16:39 +0300

    fix(reports): correct Statement of Changes in Equity label wording

--- app/src/main/java/dev/fitiavana/accounting/ui/reports/ReportType.kt
+++ app/src/main/java/dev/fitiavana/accounting/ui/reports/ReportType.kt
@@ -3,5 +3,5 @@ package dev.fitiavana.accounting.ui.reports
 enum class ReportType(val label: String) {
     BALANCE_SHEET("Balance Sheet"),
     INCOME_STATEMENT("Income Statement"),
-    CHANGES_IN_EQUITY("Statements of Changes in Equity")
+    CHANGES_IN_EQUITY("Statement of Changes in Equity")
 }
