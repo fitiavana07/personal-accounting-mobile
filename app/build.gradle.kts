5e3a09ed3cc3e159a49a1b40831a61c88451c24b
Fitiavana Ramanandafy <hello@fitiavana.dev>
2026-09-18 22:03:19 +0300

    chore: bump version to 1.24 (24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.fitiavana.accounting"
         minSdk = 19
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.23"
+        versionCode = 24
+        versionName = "1.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
